a7f9a30770af094fd8e0cf1938900606d74e6879
Carlosjw <carlos.ti.infor@gmail.com>
2021-08-30 14:30:25 -0300

    addLessonIntrodutore

---
+++ otherProject/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const element = "Eita cabrinha atoa!";
+const element2 = <h1>Encheção de linguiça!</h1>;
+
+function App() {
+  return (
+    <div className="testDiv">
+      {element}
+      {element2}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(element2, rootElement);

b9dc4e0224b21a5bd062bad19abc30bc5247ca7d
Carlosjw <carlos.ti.infor@gmail.com>
2021-09-01 15:32:24 -0300

    mergeBugLesson

---
+++ 6-webPackConfig/src/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Button = () => <button>Olá =D{a}</button>;
+
+export default Button;
--- 6-webPackConfig/webpack.config.js
+++ 6-webPackConfig/webpack.config.js
@@ -1,29 +1,28 @@
-const path = require("path");
+const path = require('path')
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // vai ser usado com o Node, por isso o module.exports
-  // mode: "production", // já estamos passando no package.json
-  entry: "./src/index.js", // ponto de entrada: onde o webpack vai pegar o arquivo para montar toda a sua árvore
+  devtool: 'source-map',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"), // __dirname: diretório raiz do projeto
-    // vai gerar uma pasta chamada 'dist', por conversão (poderia ser qualquer nome)
-    filename: "bundler.js", // arquivo final que vai ser empacotado no projeto e enviado para produção
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundler.js'
   },
   module: {
-    rules: {
-      test: /\.(js|jsx)$/, // quais tipos de arquivos serão interpretados
-      exclude: /node_modules/, // que arquivos serão excluídos
-      use: {
-        loader: "babel-loader", // qual loader será utilizado pelo webpack para a transpilação
-      },
-    },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   },
-
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html",
-    }),
-  ],
-};
+      filename: "./index.html"
+    })
+  ]
+}
